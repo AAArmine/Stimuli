84bb240fed2bfd2982f2eea41389907942a763b2
AAArmine <aaarmine.aghababyan@gmail.com>
2023-06-26 01:09:33 +0400

    initial setup

---
+++ app/posts/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostLayout = ({ children }) => {
+  return (
+    <div>
+      PostLayout
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default PostLayout;
